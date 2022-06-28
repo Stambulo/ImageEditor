705fc096e14fdbe01a71e430eb8372d4381ddaf6
Stambulo Anatolii <stambulo@yandex.ru>
2022-06-28 11:14:03 +0300

    payloads feature completed

--- app/src/main/java/com/stambulo/milestone3/view/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/stambulo/milestone3/view/adapter/GalleryAdapter.kt
@@ -2,6 +2,8 @@ package com.stambulo.milestone3.view.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -9,7 +11,7 @@ import com.stambulo.milestone3.data.MediaStoreImage
 import com.stambulo.milestone3.databinding.GalleryLayoutBinding
 
 class GalleryAdapter(val onClick: (MediaStoreImage) -> Unit) :
-    ListAdapter<MediaStoreImage, GalleryAdapter.ImageViewHolder>(MediaStoreImage.DiffCallback) {
+    ListAdapter<MediaStoreImage, GalleryAdapter.ImageViewHolder>(MediaStoreCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val view = GalleryLayoutBinding.inflate(
@@ -31,11 +33,47 @@ class GalleryAdapter(val onClick: (MediaStoreImage) -> Unit) :
             .into(holder.imageView)
     }
 
+    override fun onBindViewHolder(
+        holder: ImageViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()){
+            super.onBindViewHolder(holder, position, payloads)
+        } else {
+            if (payloads[0] == true){
+                holder.bindState(getItem(position).isChecked)
+            }
+        }
+    }
+
     inner class ImageViewHolder(
         val binding: GalleryLayoutBinding, onClick: (MediaStoreImage) -> Unit
-    ): RecyclerView.ViewHolder(binding.root) {
+    ) : RecyclerView.ViewHolder(binding.root) {
         val imageView = binding.image
 
-        fun bind(imageList: MediaStoreImage) {}
+        init {
+            imageView.setOnClickListener {
+                binding.checkEnabled.isVisible = !binding.checkEnabled.isVisible
+            }
+        }
+
+        fun bind(imageList: MediaStoreImage) {
+            binding.checkEnabled.isVisible = imageList.isChecked
+        }
+
+        fun bindState(isChecked: Boolean) {
+            binding.checkEnabled.isVisible = isChecked
+        }
+    }
+
+    class MediaStoreCallBack : DiffUtil.ItemCallback<MediaStoreImage>() {
+        override fun areItemsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
+            oldItem.id == newItem.id
+        override fun areContentsTheSame(oldItem: MediaStoreImage, newItem: MediaStoreImage) =
+            oldItem == newItem
+        override fun getChangePayload(oldItem: MediaStoreImage, newItem: MediaStoreImage): Any? {
+            return if (oldItem.id != newItem.id) true else null
+        }
     }
 }
