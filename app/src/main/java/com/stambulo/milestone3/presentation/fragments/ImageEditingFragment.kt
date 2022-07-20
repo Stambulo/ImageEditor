08d0a24ec4d8657c1da5f4ec33a41fc5e3e1da30
Stambulo Anatolii <stambulo@yandex.ru>
2022-07-20 13:19:20 +0300

    Spent a lot of time but couldn't inject dependency for navigation

--- app/src/main/java/com/stambulo/milestone3/presentation/fragments/ImageEditingFragment.kt
+++ app/src/main/java/com/stambulo/milestone3/presentation/fragments/ImageEditingFragment.kt
@@ -1,10 +1,14 @@
 package com.stambulo.milestone3.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
+import com.stambulo.milestone3.R
 import com.stambulo.milestone3.databinding.FragmentImageEditingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +22,17 @@ class ImageEditingFragment : BaseFragment<FragmentImageEditingBinding>()  {
         viewGroup: ViewGroup?
     ): FragmentImageEditingBinding {
         return FragmentImageEditingBinding.inflate(inflater, viewGroup, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Navigation.findNavController(requireActivity(), R.id.nav_host)
+                    .navigate(R.id.action_editingFragment_to_galleryFragment)
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
